f01885d63c7040459232ae5adcce47ecc1093cb5
Plunet <135955553+PlunetBusinessManager@users.noreply.github.com>
2023-06-13 11:04:19 +0200

    Update portal_type_checker.js

--- portal_type_checker.js
+++ portal_type_checker.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
     // The following code checks if the tags are within the source code of the page
     var vendorportal = document.getElementsByTagName("pl-navigation-freelancer");
-    var customerportal = document.getElementsByTagName("pl-navigation-customer");
-    if (vendorportal.length > 0) {console.log("This is the vendor portal")}
-    else if (customerportal.length > 0) { console.log("This is the customer portal")}
-    else {console.log("This is the PM Portal")};
+    var customerportal = document.getElementsByTagName("pl-navigation-customer"); // The customer portal always has this element
+    if (vendorportal.length > 0) {console.log("[My-Company Custom JavaScript]  This is the vendor portal")} // The vendor portal always has this element
+    else if (customerportal.length > 0) { console.log("[My-Company Custom JavaScript]  This is the customer portal")}
+    else {console.log("[My-Company Custom JavaScript]  This is the PM Portal")};
 });
